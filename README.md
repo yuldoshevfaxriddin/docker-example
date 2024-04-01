2ebe040723d3571bbd1ed13b67e2a6d69bc0af61
yuldoshevfaxriddin <faxriddinyuldoshev56@gmail.com>
2024-04-01 23:57:11 +0500

    docker-compose up ga tayyorgarlik

---
+++ README.md
@@ -0,0 +1,4 @@
+# docker files
+This is Docker example
+<h4>Docker containerda app lar bilan ishlash</h4>
+<p>Ikkita app yaratildi, bittasi python, bittasi php da va ular orasida o'zaro aloqa hosil qilindi</p>
---
+++ python/app.py
@@ -0,0 +1,22 @@
+# from flask import Flask
+# import json
+# app = Flask(__name__)
+
+# @app.route("/")
+# def index():
+#     return "This is python app"
+
+# @app.route("/api")
+# def api():
+#     data = []
+#     for i in range(5):
+#         person = {}
+#         person['name'] = "Yuldoshev Faxriddin"
+#         person['skils'] = ['c++','python','php']
+#         person['birdth'] = '2001 year'
+#         data.append(person)
+#     return json.dumps(data)
+
+# if __name__=='__main__':
+#     app.run(host='0.0.0.0',port=80,debug=True)
+print('salom')
